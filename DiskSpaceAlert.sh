ac97b617d65e42c1d50dc02cacbd088bc950dca2
VishuPatel <vishupatel575@gmail.com>
2024-01-24 15:19:54 -0500

    Script file has been updated.

--- DiskSpaceAlert.sh
+++ DiskSpaceAlert.sh
@@ -3,7 +3,6 @@
 # Finding out the current disk usage
 
 # Below command is used to find out the current disk utilization of the system.
-
 # first df command is used to list out all disk part usage.
 # Along other commands awk and tr are used for string manipulation.
 
@@ -13,6 +12,7 @@ DISK_SPACE=$(df -l | egrep "sda1[^(0-9)]" | awk '{print $5}' | tr -d %)
 # Sending alert email to user if any only if disk space is above certain level.
 # Here I included my email but we can add as per are requirement
 
+# 75 is the disk space limit that we can set according to our need.
 if [[ $DISK_SPACE -ge 75 ]]
 
 then
@@ -20,9 +20,6 @@ then
 
 	echo -e " !!!!!!!!!!!! Disk Space Alert !!!!!!!!!!!!!!!\n\n Your server disk space is above the certain level.\n you have to look out this immediately!!" | 
 		mail -s " DISK SPACE ALERT - ON SERVER" vjp2741.patidar@gmail.com
-
-else
-	echo "Hi $DISK_SPACE"
 fi
 
 
